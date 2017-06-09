d692d29488e372f9f616bd05a27f063e4c2d6531
Deyan <dublinstand@gmail.com>
2017-06-09 10:09:31 +0100

    empty

--- 10_hibernate_project_air_conditioner_testing_system/src/main/java/com/airconditioners/core/annotations/Insert.java
+++ 10_hibernate_project_air_conditioner_testing_system/src/main/java/com/airconditioners/core/annotations/Insert.java
@@ -1,8 +1,6 @@
 package com.airconditioners.core.annotations;
 
-
 import java.lang.annotation.*;
-
 
 //add an annotation for fields that are insertable with ElementType.FIELD
 //with this annotation we can insert the dependency manually
